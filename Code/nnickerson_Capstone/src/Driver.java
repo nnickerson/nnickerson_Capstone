9f9242ce6d7fbf17a506f9d2548cb612d7f877e7
Nathan Nickerson <nickernato@gmail.com>
2013-07-10 23:51:48 -0600

    I have successfully loaded an image using JAI.

--- Code/nnickerson_Capstone/src/Driver.java
+++ Code/nnickerson_Capstone/src/Driver.java
@@ -1,13 +1,20 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 
 import javax.media.jai.ImageJAI;
 import javax.swing.BorderFactory;
 import javax.swing.JApplet;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+
+import com.sun.media.jai.widget.DisplayJAI;
 
 /**
  * 
@@ -19,6 +26,7 @@ import javax.swing.JLabel;
  */
 public class Driver extends JApplet {
 	Image image;
+	DisplayJAI displayJAIimage;
 	public void init() {
 	    this.add(new JLabel("Look at me! I'm running!"));
 	    loadImageTest();
@@ -32,12 +40,15 @@ public class Driver extends JApplet {
 		
 		//Loading image with JAI//
 //		ImageJAI image = 
-		
+		RenderedImage renderedJAI;
+		displayJAIimage = il.loadImageWithJAI();
+		Container imageHolder = this.getContentPane();
+		imageHolder.add(new JScrollPane(displayJAIimage));
 		//End of loading image with JAI//
 	}
 	
-	public void paint(Graphics g)
-	   {
-	      g.drawImage(image, 0, 0, 1360, 768, null);
-	   } 
+//	public void paint(Graphics g)
+//	   {
+//	      g.drawImage(image, 0, 0, 1360, 768, null);
+//	   } 
 }
--- Code/nnickerson_Capstone/src/ImageLoader.java
+++ Code/nnickerson_Capstone/src/ImageLoader.java
@@ -8,29 +8,33 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.media.jai.*;
 
+import com.sun.media.jai.widget.DisplayJAI;
+
 
 public class ImageLoader {
 	String fileLocation = "C:/mcShaders2 - Copy.png";
-	public Image loadImage(AppletContext context) {
-		AppletContext appletContext = context;
-		Image image = null;
-		try {
-			File i = new File(fileLocation);
-			image = ImageIO.read(i);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return image;
-	}
-	
-//	public ImageJAI loadImageWithJAI() {
-//		ImageJAI ijai;
-//		BufferedImage bi;
-//		ParameterBlock pb = new ParameterBlock();
-//		pb.add(fileLocation);
-//		RenderedOp loadedImage = JAI.create("fileLoad", pb);
-//		bi = loadedImage.getAsBufferedImage();
-//		return ijai;
+//	public Image loadImage(AppletContext context) {
+//		AppletContext appletContext = context;
+//		Image image = null;
+//		try {
+//			File i = new File(fileLocation);
+//			image = ImageIO.read(i);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		return image;
 //	}
+	
+	public DisplayJAI loadImageWithJAI() {
+		ImageJAI ijai;
+		BufferedImage bi;
+		ParameterBlock pb = new ParameterBlock();
+		pb.add(fileLocation);
+		PlanarImage loadedImage = JAI.create("fileLoad", pb);
+		bi = loadedImage.getAsBufferedImage();
+		DisplayJAI jai = new DisplayJAI(loadedImage);
+		
+		return jai;
+	}
 }
